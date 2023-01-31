eac4888835b166586313baa99c3b81d70cf3e4f2
Sakebul-islam <mdsakebul66@gmail.com>
2023-01-31 17:35:15 +0600

    create redux trore managment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import Counter from './components/Counter';
+import { Fragment } from 'react';
 
+import { useSelector } from 'react-redux';
+
+import Header from './components/Header';
+import Auth from './components/Auth';
+import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuthencated = useSelector(state => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuthencated && <Auth />}
+      {isAuthencated && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
---
+++ src/store/counter.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter', // ( Ai name jkono deya jabe, like --> {x,y,z or a,b,c}, atar songe konokicu connect nai, akta just name)
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export const counterActions = counterSlice.actions;
+export default counterSlice.reducer;
